d2d4b6748f03293091e9d1214d2e9989ef3e0065
laybelmeyden <trycollens@gmail.com>
2020-06-17 19:29:02 +0300

    removed/docs

--- resources/views/pages/docs.blade.php
+++ resources/views/pages/docs.blade.php
@@ -3,118 +3,57 @@
 @section('content')
 
 <section>
-<div class="container">
- <div class="row">
-     <div class="col l12 s12 m12">
-         <h2 class="its_poik_rob">ДОКУМЕНТЫ</h2>
-     </div>
+  <div class="container">
+    <div class="row">
+      <div class="col l12 s12 m12">
+        <h2 class="its_poik_rob">ДОКУМЕНТЫ</h2>
+      </div>
+      <div class="col l12 s12 m12">
+        <ul style="    padding-bottom: 50px;">
+          <p class="its_1">Наши документы:</p>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
 
+            <a href="https://drive.google.com/file/d/1FFHgv72Aq8CYZdA-5-KAeyDSVbeRbEpr/view?usp=sharing" target="blank" class="edu_uli_1">Положение о конкурсном отборе Региональных Партнеров</a>
+          </li>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
 
+            <a href="https://drive.google.com/file/d/1Ck8sVxlHPiGgTTLO2eR75DUHILAIexNL/view?usp=sharing" target="blank" class="edu_uli_1">Приказ №34</a>
+          </li>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
 
+            <a href="https://drive.google.com/file/d/11VXaTYlrKanhaPwemwpHsvGjDHR1EZTJ/view?usp=sharing" target="blank" class="edu_uli_1">Методическое пособие по проведению географических фестивалей робототехники</a>
+          </li>
 
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
 
-     <div class="col l12 s12 m12">
-           <ul style="    padding-bottom: 50px;">
+            <a href="https://drive.google.com/file/d/0Bxz-EZ5tW4VbeXZ0d2w3TUVWaGc/view" target="blank" class="edu_uli_1">Отчет за 2016 год</a>
+          </li>
 
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
 
+            <a href="https://drive.google.com/file/d/1h5Ah53ufixh8KXoKplG9ozlJYCdfuOT7/view?usp=sharing" target="blank" class="edu_uli_1">Устав</a>
+          </li>
 
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
 
+            <a href="https://drive.google.com/file/d/0Bxz-EZ5tW4VbeWNHdGRleHQwems/view" target="blank" class="edu_uli_1">Свидетельство</a>
+          </li>
 
+          <!-- <li>-->
+          <!--  <img class="responsive-img edu_ul" src="assets/img/el_sin.png">-->
+          <!--  <p class="edu_uli"></p>-->
+          <!--</li>-->
 
-    <p class="its_1">Наши документы:</p>
-  
-
-    <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://vk.com/doc22052708_516634692?hash=6b3bbadd39c10f1760&dl=4baf9e95d2fbbb9931" target="blank" class="edu_uli_1">Положение о конкурсном отборе Региональных Партнеров</a>
-   </li>
-   <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://drive.google.com/file/d/1Ck8sVxlHPiGgTTLO2eR75DUHILAIexNL/view?usp=sharing" target="blank" class="edu_uli_1">Приказ №34</a>
-   </li>
-             <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://drive.google.com/file/d/11VXaTYlrKanhaPwemwpHsvGjDHR1EZTJ/view?usp=sharing" target="blank" class="edu_uli_1">Методическое пособие по проведению географических фестивалей робототехники</a>
-   </li>
-       <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://vk.com/doc22052708_477185430?hash=09fa72090250579016&dl=559b249bff7486424b" target="blank" class="edu_uli_1">Приказ № 55 о подведении итогов конкурсного отбора</a>
-   </li>      
-     <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://vk.com/doc22052708_474429089?hash=19409b0a7a33b49ffc&dl=e8128d47609af8c7a6" target="blank" class="edu_uli_1">Приказ № 54 о распределении квот между юридическими лицами по проведению конкурсного
-отбора участников «Всероссийской профильной смены по информационным
-технологиям и техническим видам спорта»</a>
-   </li>        
-             <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://vk.com/doc22052708_481464414?hash=cb60215b5e91cb6908&dl=a06c08f5b984992dc8" target="blank" class="edu_uli_1">Положение о конкурсном отборе участников "Всероссийской профильной смены по информационным технологиям и техническим видам спорта"</a>
-   </li>
-   
- <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://vk.com/doc22052708_459119123?hash=e338508ebf4877f3f2&dl=792e576538255a830c" target="blank" class="edu_uli_1">Приказ №1</a>
-   </li>
- <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://vk.com/doc158927846_451458728?hash=57c6345251a4d9aff8&dl=fef8586774af895c7e" target="blank" class="edu_uli_1">Договор оферты купли-продажи учебных комплектов</a>
-   </li>
-             
- <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://vk.com/doc158927846_450785683?hash=b21711b91f640a8067&dl=e99f93b6793b5a4db4" target="blank" class="edu_uli_1">Мониторинг развития образовательной робототехники и IT-образования в городе Москве</a>
-   </li>
-             
-
-<li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://vk.com/doc158927846_450116290?hash=b2613f1c1fcd771bcd&dl=17091c99f621a08c54" target="blank" class="edu_uli_1">Положение соревнования</a>
-   </li>             
-
-             
-<li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://vk.com/doc158927846_450116307?hash=08690b06dac690770c&dl=d0c11c7e15f6f57c82" target="blank" class="edu_uli_1">Положение Всероссийская профильная смена 2017</a>
-   </li>                 
-
-  <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://drive.google.com/file/d/0Bxz-EZ5tW4VbeXZ0d2w3TUVWaGc/view"  target="blank" class="edu_uli_1">Отчет за 2016 год</a>
-   </li>
-
-   <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://drive.google.com/file/d/1h5Ah53ufixh8KXoKplG9ozlJYCdfuOT7/view?usp=sharing" target="blank" class="edu_uli_1">Устав</a>
-   </li>
-
-   <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://drive.google.com/file/d/0Bxz-EZ5tW4VbeWNHdGRleHQwems/view" target="blank" class="edu_uli_1">Свидетельство</a>
-   </li>
-                        
-   <!-- <li>-->
-   <!--  <img class="responsive-img edu_ul" src="assets/img/el_sin.png">-->
-   <!--  <p class="edu_uli"></p>-->
-   <!--</li>-->
-
-   </ul>
-     </div>
- </div>
-</div>
+        </ul>
+      </div>
+    </div>
+  </div>
 </section>
 
 @endsection
