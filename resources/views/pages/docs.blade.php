e912e948baf56d1a943a27dd5313abddff0495ef
laybelmeyden <trycollens@gmail.com>
2020-07-20 18:09:50 +0300

    added/otchet

--- resources/views/pages/docs.blade.php
+++ resources/views/pages/docs.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.master')
 
 @section('content')
-
 <section>
   <div class="container">
     <div class="row">
@@ -13,47 +12,19 @@
           <p class="its_1">Наши документы:</p>
           <li>
             <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-            <a href="https://drive.google.com/file/d/1FFHgv72Aq8CYZdA-5-KAeyDSVbeRbEpr/view?usp=sharing" target="blank" class="edu_uli_1">Положение о конкурсном отборе Региональных Партнеров</a>
-          </li>
-          <li>
-            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-            <a href="https://drive.google.com/file/d/1Ck8sVxlHPiGgTTLO2eR75DUHILAIexNL/view?usp=sharing" target="blank" class="edu_uli_1">Приказ №34</a>
-          </li>
-          <li>
-            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-            <a href="https://drive.google.com/file/d/11VXaTYlrKanhaPwemwpHsvGjDHR1EZTJ/view?usp=sharing" target="blank" class="edu_uli_1">Методическое пособие по проведению географических фестивалей робототехники</a>
-          </li>
-
-          <li>
-            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-            <a href="https://drive.google.com/file/d/0Bxz-EZ5tW4VbeXZ0d2w3TUVWaGc/view" target="blank" class="edu_uli_1">Отчет за 2016 год</a>
-          </li>
-
-          <li>
-            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
             <a href="https://drive.google.com/file/d/1h5Ah53ufixh8KXoKplG9ozlJYCdfuOT7/view?usp=sharing" target="blank" class="edu_uli_1">Устав</a>
           </li>
-
           <li>
             <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
             <a href="https://drive.google.com/file/d/0Bxz-EZ5tW4VbeWNHdGRleHQwems/view" target="blank" class="edu_uli_1">Свидетельство</a>
           </li>
-
           <!-- <li>-->
           <!--  <img class="responsive-img edu_ul" src="assets/img/el_sin.png">-->
           <!--  <p class="edu_uli"></p>-->
           <!--</li>-->
-
         </ul>
       </div>
     </div>
   </div>
 </section>
-
 @endsection
--- resources/views/pages/otchet.blade.php
+++ resources/views/pages/otchet.blade.php
@@ -2,61 +2,53 @@
 
 @section('content')
 
-   <section>
-<div class="container">
+<section>
+  <div class="container">
     <div class="row">
-        <div class="col l12 s12 m12">
-            <h2 class="its_poik_rob">ОТЧЕТЫ</h2>
-        </div>
-
-
-
-
-
-        <div class="col l12 s12 m12">
-              <ul style="    padding-bottom: 50px;">
-
-
-              <p class="its_1">Отчет за 2018/2019 год:</p>
-
-              <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://drive.google.com/file/d/145nL0wOXCWlotH1xfyfxKOmtCbs3YR5g/view?usp=sharing" target="blank" class="edu_uli_1">Отчет о деятельности</a>
-   </li>
-   <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://drive.google.com/file/d/1OUWDgydJbdPJWX2Lz5FgwEUwVX8BllqM/view?usp=sharing" target="blank" class="edu_uli_1">Заявление о соотвествии</a>
-   </li>
-   <li>
-     <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-     <a href="https://drive.google.com/file/d/1z8PZzvl9oR08jjpaBk2VS_0vX6tMhl29/view?usp=sharing" target="blank" class="edu_uli_1">Отчет о целях расходования</a>
-   </li>
-
-       <p class="its_1">Отчет за 2016 год:</p>
-
-     <li>
-        <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
-
-        <a href="https://drive.google.com/file/d/0Bxz-EZ5tW4VbeXZ0d2w3TUVWaGc/view"  target="blank" class="edu_uli_1">Отчет за 2016 год</a>
-      </li>
-
-
-
-
-
-
-      <!-- <li>-->
-      <!--  <img class="responsive-img edu_ul" src="assets/img/el_sin.png">-->
-      <!--  <p class="edu_uli"></p>-->
-      <!--</li>-->
-
-      </ul>
-        </div>
+      <div class="col l12 s12 m12">
+        <h2 class="its_poik_rob">ОТЧЕТЫ</h2>
+      </div>
+      <div class="col l12 s12 m12">
+        <ul style="    padding-bottom: 50px;">
+        <p class="its_1">Отчет за 2019 год:</p>
+        <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
+            <a href="https://drive.google.com/file/d/1gQ4G4cmJU4FraU3uGfmi7Z6YYvb1QSzz/view?usp=sharing" target="blank" class="edu_uli_1">АИР МИНЮСТ</a>
+          </li>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
+            <a href="https://drive.google.com/file/d/1IaCjGKsUbK_b8q7f4zn8ch8xsQ24rOyO/view?usp=sharing" target="blank" class="edu_uli_1">Отчет о деятельности</a>
+          </li>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
+            <a href="https://drive.google.com/file/d/1q3_azgpJBYgJTan_wHGCLqurUKpCVDDc/view?usp=sharing" target="blank" class="edu_uli_1">Отчет о деятельности</a>
+          </li>
+          <p class="its_1">Отчет за 2018/2019 год:</p>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
+            <a href="https://drive.google.com/file/d/145nL0wOXCWlotH1xfyfxKOmtCbs3YR5g/view?usp=sharing" target="blank" class="edu_uli_1">Отчет о деятельности</a>
+          </li>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
+            <a href="https://drive.google.com/file/d/1OUWDgydJbdPJWX2Lz5FgwEUwVX8BllqM/view?usp=sharing" target="blank" class="edu_uli_1">Заявление о соотвествии</a>
+          </li>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
+            <a href="https://drive.google.com/file/d/1z8PZzvl9oR08jjpaBk2VS_0vX6tMhl29/view?usp=sharing" target="blank" class="edu_uli_1">Отчет о целях расходования</a>
+          </li>
+          <p class="its_1">Отчет за 2016 год:</p>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
+            <a href="https://drive.google.com/file/d/0Bxz-EZ5tW4VbeXZ0d2w3TUVWaGc/view" target="blank" class="edu_uli_1">Отчет за 2016 год</a>
+          </li>
+          <!-- <li>-->
+          <!--  <img class="responsive-img edu_ul" src="assets/img/el_sin.png">-->
+          <!--  <p class="edu_uli"></p>-->
+          <!--</li>-->
+        </ul>
+      </div>
     </div>
-</div>
+  </div>
 </section>
 
 @endsection
