d8e067df17ed87f94531ade23b931f656e21597e
laybelmeyden <trycollens@gmail.com>
2021-04-20 17:39:22 +0300

    added/docs

--- resources/views/pages/docs.blade.php
+++ resources/views/pages/docs.blade.php
@@ -22,6 +22,18 @@
             <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
             <a href="https://drive.google.com/file/d/0Bxz-EZ5tW4VbeWNHdGRleHQwems/view" target="blank" class="edu_uli_1">Свидетельство</a>
           </li>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
+            <a href="/assets/files/on0001t.xls" target="blank" class="edu_uli_1">Сведения о персональном составе руководящих органов некоммерческой организации</a>
+          </li>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
+            <a href="/assets/files/on0002t.xls" target="blank" class="edu_uli_1">Отчет о целях расходования некоммерческой организацией денежных средств и использования</a>
+          </li>
+          <li>
+            <img class="responsive-img edu_ul" src="assets/img/el_sin.png">
+            <a href="/assets/files/Zayavlenie.xls" target="blank" class="edu_uli_1">Сообщение о продолжении деятельности </a>
+          </li>
           <!-- <li>-->
           <!--  <img class="responsive-img edu_ul" src="assets/img/el_sin.png">-->
           <!--  <p class="edu_uli"></p>-->
